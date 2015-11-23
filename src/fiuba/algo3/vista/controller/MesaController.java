50a12002ea439a7f8346d7577e71858b42ad468d
edhernandez <hernandezed.1991@gmail.com>
2015-11-23 01:24:28 -0300

    agrego rotacion de jugadores a nivel visual y seleccion visual de cartas

--- src/fiuba/algo3/vista/controller/MesaController.java
+++ src/fiuba/algo3/vista/controller/MesaController.java
@@ -1,13 +1,17 @@
 package fiuba.algo3.vista.controller;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
-
+import fiuba.algo3.vista.controller.handler.CartaHandler;
 import fiuba.algo3.vista.controller.handler.btnSalirHandler;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 public class MesaController extends Controller{
 	
@@ -28,12 +32,48 @@ public class MesaController extends Controller{
 	@FXML
     private Button btnFlor;
 	
+	@FXML
+    private ImageView carta1Jug1;
+	@FXML
+    private ImageView carta2Jug1;
+	@FXML
+    private ImageView carta3Jug1;
+	@FXML
+    private ImageView carta1Jug2;
+	@FXML
+    private ImageView carta2Jug2;
+	@FXML
+    private ImageView carta3Jug2;
+	@FXML
+    private ImageView carta1Jug3;
+	@FXML
+    private ImageView carta2Jug3;
+	@FXML
+    private ImageView carta3Jug3;
+	@FXML
+    private ImageView carta1Jug4;
+	@FXML
+    private ImageView carta2Jug4;
+	@FXML
+    private ImageView carta3Jug4;
+	
+	private int cantidadDeJugadores = 4; //sacar el hardcodeo!!!!!!!!!!
+	
+	private int numeroCartaJugador = 0;
+	
+	private List<ImageView> cartasEnMano;
 	
 	@Override
 	public void initialize(URL url, ResourceBundle resources) {
+		 cartasEnMano = new ArrayList<>(Arrays.asList(carta1Jug1,carta2Jug1,carta3Jug1,
+				carta1Jug2,carta2Jug2,carta3Jug2,
+				carta1Jug3,carta2Jug3,carta3Jug3,
+				carta1Jug4,carta2Jug4,carta3Jug4));
 		btnSalirHandler();
 		btnAyudaHandler();
 		btnVolverHandler();
+		cartasHandler();
+		btnJugarCartaHandler();
 	}
 	
 	public void btnSalirHandler() {
@@ -62,7 +102,7 @@ public class MesaController extends Controller{
 		btnJugarCarta.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				// TODO Auto-generated method stub
+				rotarJugador();
 			}
 		});
 	}
@@ -89,5 +129,29 @@ public class MesaController extends Controller{
 				// TODO Auto-generated method stub
 			}
 		});
+	}
+	
+	public void cartasHandler() {
+		for (ImageView carta : cartasEnMano) {
+			carta.setOnMouseClicked(new CartaHandler(carta, cartasEnMano));
+		}
+	}
+	
+	public void rotarJugador() {
+		habilitarCarta();
+		numeroCartaJugador= numeroCartaJugador+3;
+		if(numeroCartaJugador>11){
+			numeroCartaJugador=0;
+		}
+		
+		habilitarCarta();
+	
+	}
+	
+	public void habilitarCarta() {
+		for(int i=numeroCartaJugador;i<numeroCartaJugador+3;i++){
+			cartasEnMano.get(i).setDisable(!cartasEnMano.get(i).isDisable());
+		}
+		
 	}
 }
---
+++ src/fiuba/algo3/vista/controller/handler/CartaHandler.java
@@ -0,0 +1,31 @@
+package fiuba.algo3.vista.controller.handler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+
+public class CartaHandler implements EventHandler<Event> {
+
+	private List<ImageView> cartasEnMano;
+	
+	private ImageView cartaSeleccionada;
+	
+	public CartaHandler(ImageView cartaSeleccionada, List<ImageView> cartasEnMano) {
+		this.cartaSeleccionada = cartaSeleccionada;
+		this.cartasEnMano = new ArrayList<>(cartasEnMano);
+		this.cartasEnMano.remove(cartaSeleccionada);
+	}
+	
+	@Override
+	public void handle(Event event) {
+		 cartaSeleccionada.setEffect(new DropShadow(20, Color.BLACK));
+		 for(ImageView cartaEnMano : cartasEnMano) {
+			 cartaEnMano.setEffect(null);
+		 }
+	}
+}
